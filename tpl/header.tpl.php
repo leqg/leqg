e17962a7661330c009a1a6d889f21181a1c9b2e7
Hiwelo <damien@bretzel-et-bredele.fr>
2014-07-29 18:49:40 +0200

    Réactivation des Google Fonts

--- tpl/header.tpl.php
+++ tpl/header.tpl.php
@@ -7,7 +7,7 @@
 	<!-- lien vers les feuilles de style -->
 	<link rel="stylesheet" media="all" href="assets/css/style.css">
 	<link rel="stylesheet" media="all" href="assets/css/fonts.css">
-	<!--<link href='http://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic,500,500italic,700,700italic,900,900italic&subset=latin-ext' rel='stylesheet' type='text/css'>-->
+	<link href='http://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic,500,500italic,700,700italic,900,900italic&subset=latin-ext' rel='stylesheet' type='text/css'>
 
 	<!-- lien vers les ressources Javascript / jQuery -->
 	<script src="assets/js/jquery-2.1.1.min.js"></script>
