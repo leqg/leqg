4cb2aa1775df1df986a2f4483d4161ea5f5c2aaf
Hiwelo <damien@bretzel-et-bredele.fr>
2014-08-01 12:50:43 +0200

    Classe Historique – Améliorations
    Mise en place de méthodes pour rechercher tous les éléments
    d’historique reliés à une fiche et les dénombrer

--- class/historique.class.php
+++ class/historique.class.php
@@ -13,4 +13,42 @@ class historique extends core {
 		$this->db = $db;
 		$this->compte = $compte;
 	}
+	
+	
+	// rechercheParFiche( int ) permet d'extraire de la BDD les entrées de l'historique pour une fiche demandée
+	public	function rechercheParFiche( $fiche ) {
+		// On vérifie que la fiche est bien un nombre (id = numeric)
+		if ( is_numeric( $fiche ) ) :
+		
+			// On effectue la recherche dans la BDD des entrées dans l'historique rattachées à la fiche contact demandée
+			$query = 'SELECT * FROM historique WHERE contact_id = ' . $fiche;
+			$sql = $this->db->query($query);
+			
+			// On fait la liste de toutes les entrées pour les affecter dans un tableau
+			$entrees = array();
+			
+			while ( $row = $sql->fetch_assoc() ) $entrees[] = $row;
+			
+			return $entrees;
+		
+		else : return false; endif;
+	}
+	
+	
+	// nombre( int ) permet d'extraire le nombres d'entrées dans l'historique du compte rattachés à la fiche contact demandée
+	public	function nombre( $fiche ) {
+		// On vérifie que la fiche est bien un nombre
+		if ( is_numeric( $fiche ) ) :
+		
+			// On charge les entrées relatives à la fiche contact
+			$entrees = $this->rechercheParFiche( $fiche );
+			
+			// On compte le nombre d'entrées
+			$nombre = count($entrees);
+			
+			// On retourne le nombre trouvé
+			return $nombre;
+		
+		else : return false; endif;
+	}
 }
