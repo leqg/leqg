afa5266cd675187d95a8ec53e9fc0620789219dc
Hiwelo <damien@bretzel-et-bredele.fr>
2014-08-07 12:26:54 +0200

    Design – Ajout Fira
    Ajout d’une nouvelle typographie permettant l’amélioration de la
    lisibilité des textes longs (prises de notes, résumés d’entretiens,
    etc.)

--- tpl/header.tpl.php
+++ tpl/header.tpl.php
@@ -8,6 +8,7 @@
 	<link rel="stylesheet" media="all" href="assets/css/style.css">
 	<link rel="stylesheet" media="all" href="assets/css/fonts.css">
 	<link href='http://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic,500,500italic,700,700italic,900,900italic&subset=latin-ext' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Fira+Sans:300,300italic,400,400italic,500,500italic,700,700italic&subset=latin-ext' rel='stylesheet' type='text/css'>
 
 	<!-- lien vers les ressources Javascript / jQuery -->
 	<script src="assets/js/jquery-2.1.1.min.js"></script>
