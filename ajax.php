9f6ee11886ce12bbc4b20180f41c178f7e34ec43
Hiwelo <damien@bretzel-et-bredele.fr>
2014-08-10 15:41:19 +0200

    AJAX – Amélioration de l'appel des scripts
    La gestion des scripts appelés par la page d’appel est maintenant
    automatique, se basant sur les fichiers .ajax.php créés dans le dossier
    ajax/

--- ajax.php
+++ ajax.php
@@ -8,15 +8,31 @@
 	require_once 'includes.php';
 
 
+// On initialise le tableau listant les scripts AJAX créés
+	$scripts = array();
+
+// On tente d'ouvrir le dossier AJAX pour connaître le contenu des appels AJAX créés
+	if ($dossier = opendir('./ajax')) :
+	
+	// On vérifie que l'ouverture et la lecture du dossier n'a pas retourné d'erreur
+		while (false !== ($fichier = readdir($dossier))) :
+					
+		// On analyse le nom du fichier
+			$fichier = explode('.', $fichier);
+			
+		// On vérifie que le fichier est bien un script .ajax.php
+			if ($fichier[1] == 'ajax' && $fichier[2] == 'php') :
+			
+			// Si oui, on rajoute le script à la liste des scripts
+				$scripts[] = $fichier[0];
+			
+			endif;
+		
+		endwhile;
+	
+	endif;
+
 // On fait la liste des différents scripts pouvant être appelés ci-après
-	$scripts = array(	'historique-ajout',
-						'interaction-chargement',
-						'fichier-envoi',
-						'recherche-ville',
-						'recherche-rue',
-						'recherche-immeuble',
-						'nouvelle-adresse',
-					);
 
 // On vérifie que le script demandé existe
 	$script = $core->securisation_string($_GET['script']);
