3ba5541db7fae5cd53ccc459281c0c431a461c06
Hiwelo <damien@bretzel-et-bredele.fr>
2014-08-11 23:34:09 +0200

    CSV – Méthode de lecture de fichier
    Méthode, correction de la classe et fichier de test

--- class/csv.class.php
+++ class/csv.class.php
@@ -5,21 +5,49 @@
 */
 
 
-class carto extends core {
+class csv extends core {
 	
 // Définition des propriétés
 	private $db; // Lien vers la base MySQL
-	private $compte; // ID du compte qui utilise à l'instant donné la plateforme
 	
 	
 // Définition des méthodes
 	
-	public	function __construct($db, $compte) {
+	public	function __construct($db) {
 		$this->db = $db;
-		$this->compte = $compte;
 	}
 		
 	
 // Méthodes liées à la classe sélectionnée
 	
+	// lectureFichier( string , string ) permet de traiter les données d'un fichier CSV dans un tableau
+	public	function lectureFichier( $fichier , $separateur = ';' ) {
+		
+		// On défini les données de démarrage
+		$row = 0;
+		$line = array();
+		$data = array();
+		$head = array();
+		
+		// On ouvre le fichier, uniquement en lecture
+		$file = fopen($fichier, 'r');
+		
+		// On calcule la taille du fichier
+		$size = filesize($fichier) + 1;
+		
+		// On fait une boucle pour chaque ligne du fichier
+		while ($line = fgetcsv($file, $size, $separateur)) :
+		
+			// On affecte les données de la ligne au tableau général
+			$data[$row] = $line;
+			$row++;
+		
+		endwhile;
+		
+		// On ferme le fichier
+		fclose($file);
+		
+		// On retourne le tableau contenant les données du fichier
+		return $data;
+	}
 }
--- csv.php
+++ csv.php
@@ -2,4 +2,39 @@
 
 require_once 'includes.php';
 
-?>
+$data = $csv->lectureFichier('insee.csv');
+
+$row = 0;
+foreach ($data as $line) {
+	/*
+		[0] => Commune
+        [1] => Codepos
+        [2] => Departement
+        [3] => INSEE	
+	*/
+	
+	// On vérifie qu'il ne s'agit pas de la première ligne
+	if ($row > 0) :	
+		// On cherche l'ID de la commune correspondante
+		$query = 'SELECT commune_id FROM communes WHERE CONCAT(departement_id, commune_numero) = ' . $line[3];
+		$sql = $db->query($query);
+		if ($sql->num_rows == 1) :
+			$row = $sql->fetch_array();
+			$id = $row[0];
+			
+			// On enregistre le code postal dans la base de données
+			$query = 'INSERT INTO codes_postaux (code_postal, commune_id) VALUES (' . $line[1] . ', ' . $id . ')';
+			$db->query($query);
+		else :
+			// On enregistre le code postal dans la base de données sans la ville
+			$query = 'INSERT INTO codes_postaux (code_postal, commune_id) VALUES (' . $line[1] . ', 0)';
+			$db->query($query);
+		endif;
+		
+		$row++;
+	
+	// Sinon, on la saute
+	else : $row++; endif;
+}
+
+?>Fin!
