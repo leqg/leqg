cf46f136305caa5e380449ebefbe6f653fc76390
Hiwelo <mail@damiensenger.me>
2014-08-14 17:16:49 +0200

    Mise en place de la nouvelle feuille CSS

--- tpl/header.tpl.php
+++ tpl/header.tpl.php
@@ -5,7 +5,7 @@
 	<title>LeQG : gestion de base de données électorale</title>
 	
 	<!-- lien vers les feuilles de style -->
-	<link rel="stylesheet" media="all" href="assets/css/style.css">
+	<link rel="stylesheet" media="all" href="assets/css/main.css">
 	<link rel="stylesheet" media="all" href="assets/css/fonts.css">
 	<link href='http://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic,500,500italic,700,700italic,900,900italic&subset=latin-ext' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Fira+Sans:300,300italic,400,400italic,500,500italic,700,700italic&subset=latin-ext' rel='stylesheet' type='text/css'>
