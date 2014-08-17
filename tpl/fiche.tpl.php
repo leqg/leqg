46eeba58e27a0e6b281089c3aae85a1a139ce2a4
Hiwelo <mail@damiensenger.me>
2014-08-18 00:56:47 +0200

    Correction du lien de modification des coordonnées
    Fix #8

--- tpl/fiche.tpl.php
+++ tpl/fiche.tpl.php
@@ -25,7 +25,7 @@
 			<li>
 				<span class="label-information">Adresse déclarée</span>
 				<p><?php $carto->adressePostale($fiche->get_immeuble()); ?></p>
-				<span class="icone-modification" id="modifierAdressePostale" title="Modifier l'adresse déclarée"><a href="<?php $core->tpl_go_to('fiche', array('id' => $_GET['id'])); ?>&modifierAdresse=true">&#xe855;</a></span>
+				<span class="icone-modification"><a title="Modifier l'adresse déclarée" href="<?php $core->tpl_go_to('fiche', array('id' => $_GET['id'])); ?>&modifierAdresse=true">&#xe855;</a></span>
 			</li>
 			<li>
 				<span class="label-information">Bureau de vote</span>
