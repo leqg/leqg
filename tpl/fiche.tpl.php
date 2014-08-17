aead4b311bee533196776665dca186eace95a9c2
Hiwelo <mail@damiensenger.me>
2014-08-18 01:06:14 +0200

    Corrections style CSS fiches – fix #8

--- tpl/fiche.tpl.php
+++ tpl/fiche.tpl.php
@@ -25,7 +25,7 @@
 			<li>
 				<span class="label-information">Adresse déclarée</span>
 				<p><?php $carto->adressePostale($fiche->get_immeuble()); ?></p>
-				<a title="Modifier l'adresse déclarée" href="<?php $core->tpl_go_to('fiche', array('id' => $_GET['id'])); ?>&modifierAdresse=true"><span class="icone-modification">&#xe855;</span></a>
+				<a class="nostyle" title="Modifier l'adresse déclarée" href="<?php $core->tpl_go_to('fiche', array('id' => $_GET['id'])); ?>&modifierAdresse=true"><span class="icone-modification">&#xe855;</span></a>
 			</li>
 			<li>
 				<span class="label-information">Bureau de vote</span>
