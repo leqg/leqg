873fec3d06eab21e18ddd54b3a60ab4e9098fd1e
Hiwelo <mail@damiensenger.me>
2014-08-18 13:21:11 +0200

    Ajout des liens sur le menu principal

--- tpl/services.tpl.php
+++ tpl/services.tpl.php
@@ -1,14 +1,14 @@
 <table id="services">
 	<tr>
-		<td><a href=""><span>&#xe840;</span><p>Contacts</p></a></td>
-		<td><a href=""><span>&#xe851;</span><p>Dossiers</p></a></td>
-		<td><a href=""><span>&#xe845;</span><p>Cartographie</p></a></td>
-		<td><a href=""><span>&#xe841;</span><p>Porte à porte</p></a></td>
+		<td><a href="<?php $core->tpl_go_to('contacts'); ?>"><span>&#xe840;</span><p>Contacts</p></a></td>
+		<td><a href="<?php $core->tpl_go_to('dossiers'); ?>"><span>&#xe851;</span><p>Dossiers</p></a></td>
+		<td><a href="<?php $core->tpl_go_to('carto'); ?>"><span>&#xe845;</span><p>Cartographie</p></a></td>
+		<td><a href="<?php $core->tpl_go_to('porte'); ?>"><span>&#xe841;</span><p>Porte à porte</p></a></td>
 	</tr>
 	<tr>
-		<td><a href=""><span>&#xe84d;</span><p>Diffusion</p></a></td>
-		<td><a href=""><span>&#xe8ef;</span><p>Publipostage</p></a></td>
-		<td><a href=""><span>&#xe805;</span><p>Emailing</p></a></td>
-		<td><a href=""><span>&#xe8e4;</span><p>SMS</p></a></td>
+		<td><a href="<?php $core->tpl_go_to('boite'); ?>"><span>&#xe84d;</span><p>Diffusion</p></a></td>
+		<td><a href="<?php $core->tpl_go_to('poste'); ?>"><span>&#xe8ef;</span><p>Publipostage</p></a></td>
+		<td><a href="<?php $core->tpl_go_to('email'); ?>"><span>&#xe805;</span><p>Emailing</p></a></td>
+		<td><a href="<?php $core->tpl_go_to('sms'); ?>"><span>&#xe8e4;</span><p>SMS</p></a></td>
 	</tr>
 </table>
