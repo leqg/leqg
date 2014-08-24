0aea50e455d17de19be022049405cc00a7c6cf09
Hiwelo <mail@damiensenger.me>
2014-08-24 18:42:29 +0200

    Amélioration affichage des dernières interactions

--- tpl/contact-interactions.tpl.php
+++ tpl/contact-interactions.tpl.php
@@ -8,21 +8,16 @@
 
 	<h3>Derniers événements enregistrés dans le QG</h3>
 	
-	<ul class="deuxColonnes">
-		<li>
-			<span class="label-information">Électeurs concernés</span>
-			<ul class="listeEncadree">
-				<?php foreach($interactions as $interaction) : ?>
-				<a class="nostyle" href="<?php $core->tpl_go_to('fiche', array('id' => $interaction['contact_id'], 'interaction' => $interaction['id'])); ?>">
-					<li class="electeur">
-						<strong><?php $fiche->affichageNomById($interaction['contact_id']); ?></strong>
-						<p class="tailleNormale">
-							<em><?php echo date('d/m/Y', strtotime($interaction['date'])); ?> &bull; <?php $historique->returnType($interaction['type']); ?></em> – <?php echo $interaction['objet']; ?>
-						</p>
-					</li>
-				</a>
-				<?php endforeach; ?>
-			</ul>
-		</li>
-	
+	<ul class="listeEncadree seul">
+		<?php foreach($interactions as $interaction) : ?>
+		<a class="nostyle" href="<?php $core->tpl_go_to('fiche', array('id' => $interaction['contact_id'], 'interaction' => $interaction['id'])); ?>">
+			<li class="electeur">
+				<strong><?php $fiche->affichageNomById($interaction['contact_id']); ?></strong>
+				<p class="tailleNormale">
+					<em><?php echo date('d/m/Y', strtotime($interaction['date'])); ?> &bull; <?php $historique->returnType($interaction['type']); ?></em> – <?php echo $interaction['objet']; ?>
+				</p>
+			</li>
+		</a>
+		<?php endforeach; ?>
+	</ul>	
 </section>
