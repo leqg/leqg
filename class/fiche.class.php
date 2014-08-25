9ba61aa4982b1dea1c5a536dbbe85fc52c254e65
Hiwelo <mail@damiensenger.me>
2014-08-26 01:35:01 +0200

    Export – debug fix #25

--- class/fiche.class.php
+++ class/fiche.class.php
@@ -850,13 +850,13 @@ class fiche extends core {
 				$immeuble = $this->formatage_donnees($immeuble->fetch_assoc());
 				
 				$rue = $this->db->query('SELECT * FROM rues WHERE rue_id = ' . $immeuble['rue_id']);
-				if ($rue->num_rows > 0) : $rue = $this->formatage_donnees($rue->fetch_assoc()); else: $rue['nom'] = '';
+				if ($rue->num_rows > 0) : $rue = $this->formatage_donnees($rue->fetch_assoc()); else: $rue['nom'] = ''; endif;
 				
 				$ville = $this->db->query('SELECT * FROM communes WHERE commune_id = ' . $rue['commune_id']);
-				if ($ville->num_rows > 0) : $ville = $this->formatage_donnees($ville->fetch_assoc()); else: $ville['nom'] = '';
+				if ($ville->num_rows > 0) : $ville = $this->formatage_donnees($ville->fetch_assoc()); else: $ville['nom'] = ''; endif;
 				
 				$cp = $this->db->query('SELECT * FROM codes_postaux WHERE commune_id = ' . $ville['id']);
-				if ($cp->num_rows > 0) : $cp->fetch_assoc(); else: $cp['code_postal'] = '';
+				if ($cp->num_rows > 0) : $cp->fetch_assoc(); else: $cp['code_postal'] = ''; endif;
 				
 				// on rassemble les informations qu'on balance dans le fichier
 				$ligne = array($contact['contact_nom'],
