3ad77bf901259541c4ad5f2eae9d2d572f52e3cc
Hiwelo <mail@damiensenger.me>
2014-08-26 00:51:31 +0200

    Debug – Améliorations

--- class/core.class.php
+++ class/core.class.php
@@ -24,7 +24,7 @@ class core {
 	
 	// debug( array ) permet d'afficher un array mis en forme par la fonction print_r et d'arrêter le script à des fins de debug
 		public	function debug($array, $htmlentities = false) {
-			echo '<pre>';
+			echo '<pre class="nowrap">';
 		
 			// On affiche le tableau préformaté s'il s'agit d'un tableau
 			if (is_array($array)) { print_r($array); }
