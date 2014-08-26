b66a70f0bd424ba129b42bdb17058464de6270b2
Hiwelo <mail@damiensenger.me>
2014-08-26 23:16:36 +0200

    Dossiers — retrait possibilité création de dossier
    Close #38

--- tpl/menu-creation.tpl.php
+++ tpl/menu-creation.tpl.php
@@ -1,4 +1,4 @@
 <ul id="creationsRapides">
 	<a href="<?php $core->tpl_get_url('fiche' , 'creation' , 'action'); ?>"><li class="creationFiche">Nouvelle fiche</li></a>
-	<a href="#"><li class="creationDossier">Nouveau dossier</li></a>
+	<!--<a href="#"><li class="creationDossier">Nouveau dossier</li></a>-->
 </ul>
