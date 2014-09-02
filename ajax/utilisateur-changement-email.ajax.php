e0f96701f599a42cb2ec9e568b409e8a46c37d99
Hiwelo <mail@damiensenger.me>
2014-09-02 15:25:54 +0200

    Utilisateur – système de changement d'email

---
+++ ajax/utilisateur-changement-email.ajax.php
@@ -0,0 +1,44 @@
+<?php
+
+	// On récupère les informations envoyées
+	$donnees = $_POST;
+	
+	// On fabrique le hash de validation
+	$hash = sha1($donnees['email'] . time());
+	
+	// On enregistre le nouvel email dans la base de données en attendant qu'il soit validé
+	$query = 'UPDATE `users` SET `user_new_email` = "' . $donnees['email'] . '", `user_new_email_hash` = "' . $hash . '" WHERE `user_id` = ' . $donnees['user'];
+	$noyau->query($query);
+	
+	// On fait partir le mail permettant de valider le changement d'email
+	$email = file_get_contents('tpl/mail/changement-email.tpl.html');
+	$objet = 'LeQG – Changement de l\'adresse email de votre compte.';
+	
+	// On insère dans le mail l'URL du fichier pour qu'il puisse être téléchargé
+	$email = strtr($email, array('{USER}' => $user->get_the_nickname()));
+	$email = strtr($email, array('{URL}' => 'http://' . $config['SERVER']['url'] . '/validation-email.php?email=' . $hash));
+	
+	// On démarre l'instance
+	$mail = new PHPMailer();
+	
+	// On contacte le serveur d'envoi SMTP
+	$mail->IsSMTP();
+	$mail->SMTPAuth = true;
+	$mail->Host = $api['mail']['smtp']['host'];
+	$mail->Port = $api['mail']['smtp']['port'];
+	$mail->Username = $api['mail']['smtp']['user'];
+	$mail->Password = $api['mail']['smtp']['pass'];
+
+	// On configure le mail à envoyer
+	$mail->CharSet = $api['mail']['charset'];
+	$mail->SetFrom('noreply@leqg.info', 'LeQG');
+	$mail->AddReplyTo('tech@leqg.info', 'LeQG équipe technique');
+	$mail->AddAddress($donnees['email'], $user->get_the_nickname());
+	$mail->Subject = $objet;
+	$mail->MsgHTML($email);
+
+	$mail->Send();
+	
+	// On redirige vers la page de profil avec un message spécifique
+	$core->tpl_go_to('utilisateur', array('message' => 'changement-email'), true); 
+?>
--- tpl/utilisateur.tpl.php
+++ tpl/utilisateur.tpl.php
@@ -11,11 +11,11 @@
 		<li>
 			<span class="label-information">Email</span>
 			<p><?php $user->the_email(); ?></p>
-			<a class="nostyle icone" title="Modifier l'email de contact et de connexion" href="<?php $core->tpl_go_to('utilisateur', array('changerEmail' => 'true')); ?>">&#xe855;</a>
+			<a class="nostyle icone" title="Modifier l'email de contact et de connexion" href="<?php $core->tpl_go_to('utilisateur', array('modification' => 'email')); ?>">&#xe855;</a>
 		</li>
 		<li>
 			<span class="label-information">Mot de passe</span>
-			<p><a href="<?php $core->tpl_go_to('utilisateur', array('changerPass' => 'true')); ?>">Mettre à jour votre mot de passe</a></p>
+			<p><a href="<?php $core->tpl_go_to('utilisateur', array('modification' => 'pass')); ?>">Mettre à jour votre mot de passe</a></p>
 		</li>
 		<li>
 			<span class="label-information"><label for="form-mobile">Téléphone</label></span>
@@ -31,29 +31,68 @@
 </section>
 
 <aside>
-	<div>
-		<h6>Historique des dernières connexions</h6>
+	<?php if (isset($_GET['modification'])) : ?>
+		<?php if ($_GET['modification'] == 'email') : ?>
+			<div>
+				<h6>Modification de l'email associé au compte</h6>
+				<p>
+					Attention, la connexion au service LeQG utilise votre email comme identifiant et comme information principale de contact.
+					<strong>Veillez à entrer un email valide afin de ne pas perdre l'accès à votre compte.</strong>
+				</p>
+				
+				<form action="ajax.php?script=utilisateur-changement-email" method="post">
+					<input type="hidden" name="user" value="<?php $user->the_id(); ?>">
+					<ul class="deuxColonnes">
+						<li>
+							<span class="label-information">Email actuel</span>
+							<p><?php $user->the_email(); ?></p>
+						</li>
+						<li>
+							<span class="label-information"><label for="form-email">Nouvel email</label></span>
+							<input type="email" id="form-email" name="email" placeholder="abc@domaine.fr">
+						</li>
+						<li class="submit">
+							<input type="submit" value="Changer mon email">
+						</li>
+					</ul>
+				</form>
+			</div>
+		<?php elseif ($_GET['modification'] == 'pass') : ?>
 		
-		<?php $query = 'SELECT * FROM `connexions` WHERE `user_id` = ' . $user->get_the_id() . ' ORDER BY connexion_date DESC LIMIT 0, 30'; $sql = $noyau->query($query); while($row = $sql->fetch_assoc()) $connexions[] = $core->formatage_donnees($row); ?>
-	
-		<table id="historique-contact">
-			<thead>
-				<tr>
-					<th width="200px">Plateforme</th>
-					<th width="300px">Date</th>
-					<th>Adresse IP</th>
-					<!--<th>Host</th>-->
-				</tr>
-			</thead>
-			<tbody>
-				<?php foreach ($connexions as $connexion) : ?>
-				<tr>
-					<td><?php echo ($connexion['plateforme']) ? 'Internet' : 'Mobile'; ?></td>
-					<td><?php echo ucwords(strftime('%A %e %B %Y %X', strtotime($connexion['date']))); ?></td>
-					<td><?php echo $connexion['ip']; ?></td>
-				</tr>
-				<?php endforeach; ?>
-			</tbody>
-		</table>
-	</div>
+		<?php endif; ?>
+	<?php else : ?>
+		<div>
+		<?php if (isset($_GET['message'])) : ?>
+			<?php if ($_GET['message'] == 'changement-email') : ?>
+				<div class="message orange">
+					<strong>Changement de votre email en cours</strong>
+					<p>Vous allez recevoir un email contenant un lieu sur lequel nous vous invitons à cliquer pour valider le changement de votre adresse email.</p>
+				</div>
+			<?php endif; ?>
+		<?php endif; ?>
+			<h6>Historique des dernières connexions</h6>
+			
+			<?php $query = 'SELECT * FROM `connexions` WHERE `user_id` = ' . $user->get_the_id() . ' ORDER BY connexion_date DESC LIMIT 0, 30'; $sql = $noyau->query($query); while($row = $sql->fetch_assoc()) $connexions[] = $core->formatage_donnees($row); ?>
+		
+			<table id="historique-contact">
+				<thead>
+					<tr>
+						<th width="200px">Plateforme</th>
+						<th width="300px">Date</th>
+						<th>Adresse IP</th>
+						<!--<th>Host</th>-->
+					</tr>
+				</thead>
+				<tbody>
+					<?php foreach ($connexions as $connexion) : ?>
+					<tr>
+						<td><?php echo ($connexion['plateforme']) ? 'Internet' : 'Mobile'; ?></td>
+						<td><?php echo ucwords(strftime('%A %e %B %Y %X', strtotime($connexion['date']))); ?></td>
+						<td><?php echo $connexion['ip']; ?></td>
+					</tr>
+					<?php endforeach; ?>
+				</tbody>
+			</table>
+		</div>
+	<?php endif; ?>
 </aside>
