9dfb1a82abbc247533e3ffc16c5ae9142893eda3
Hiwelo <mail@damiensenger.me>
2014-09-04 14:00:49 +0200

    Update recherche de fiches par thématique

--- ajax/recherche-fiches.ajax.php
+++ ajax/recherche-fiches.ajax.php
@@ -7,12 +7,12 @@
 	$query = 'SELECT	*
 			  FROM		contacts
 			  WHERE		contact_tags LIKE "%' . $recherche . '%"
-			  ORDER BY	contact_nom, contact_nom_usage, contact_prenoms ASC';
+			  ORDER BY	contact_nom, contact_nom_usage, contact_prenoms ASC LIMIT 0,30';
 			  
 	$sql = $db->query($query);
 	
 	if ($sql->num_rows > 0) :
-		while ($contacts = $sql->fetch_assoc()) : $contact = $core->formatage_donnees($dossiers);
+		while ($contacts = $sql->fetch_assoc()) : $contact = $core->formatage_donnees($contacts);
 ?>
 <a href="<?php $core->tpl_go_to('contact', array('id' => $contact['id'])); ?>">
 	<li class="electeur">
