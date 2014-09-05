93149d42f93a13d1b2af5ee5926dd6b30fb79cb1
Hiwelo <mail@damiensenger.me>
2014-09-05 13:04:31 +0200

    SQL Install Update

--- ajax/admin/installation-bdd.ajax.php
+++ ajax/admin/installation-bdd.ajax.php
@@ -6,10 +6,10 @@ $sql = file_get_contents('squelette.sql');
 // On extrait toutes les requêtes SQL
 $req = explode(';', $sql);
 
+// On initialise la gestion des erreurs
+$erreurs = array();
+
 // On exécute toutes les requêtes
-foreach ($req as $r) {
-	$db->query($r);
-}
+foreach ($req as $r) $db->query($r);
 
 ?>
-Fini!
