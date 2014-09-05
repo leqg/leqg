d90e09a126ecb71d86f95d57df729139aa2a0de7
Hiwelo <mail@damiensenger.me>
2014-09-05 13:04:13 +0200

    SQL Dump

--- squelette.sql
+++ squelette.sql
@@ -1,6 +1,3 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
 CREATE TABLE `arrondissements` (
   `arrondissement_id` int(4) unsigned NOT NULL AUTO_INCREMENT,
   `arrondissement_numero` smallint(2) unsigned zerofill NOT NULL,
