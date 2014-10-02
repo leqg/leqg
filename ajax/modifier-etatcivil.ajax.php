9bc2aec3c377eb36a294229ef76687dcfb62dd71
Hiwelo <mail@damiensenger.me>
2014-10-02 13:21:53 +0200

    Contacts – modification des données d'état civil
    Correction d’un problème à la suppression d’une donnée d’état-civil
    (fix #96)

--- ajax/modifier-etatcivil.ajax.php
+++ ajax/modifier-etatcivil.ajax.php
@@ -1,51 +1,20 @@
 <?php
 	
-	// On traite les informations
-	if (!empty($_POST['nom'])) :
-	
-		// On formate l'information
+	// On récupère les informations entrées
+	if ($_POST) {
 		$nom = $core->securisation_string($_POST['nom']);
-
-		// On prépare la requête
-		$query = 'UPDATE		contacts
-				  SET		contact_nom = "' . $nom . '"
-				  WHERE		contact_id = ' . $_GET['fiche'];
-		
-		// On exécute la requête
-		$db->query($query);
-
-	endif;
-	
-	if (!empty($_POST['nomUsage'])) :
-	
-		// On formate l'information
 		$nomUsage = $core->securisation_string($_POST['nomUsage']);
-
-		// On prépare la requête
-		$query = 'UPDATE		contacts
-				  SET		contact_nom_usage = "' . $nomUsage . '"
-				  WHERE		contact_id = ' . $_GET['fiche'];
-		
-		// On exécute la requête
-		$db->query($query);
-
-	endif;
-	
-	if (!empty($_POST['prenoms'])) :
-	
-		// On formate l'information
 		$prenoms = $core->securisation_string($_POST['prenoms']);
-
-		// On prépare la requête
-		$query = 'UPDATE		contacts
-				  SET		contact_prenoms = "' . $prenoms . '"
-				  WHERE		contact_id = ' . $_GET['fiche'];
 		
+		// On prépare la requête
+		$query = 'UPDATE	contacts
+				  SET		contact_nom = "' . $nom . '",
+				  			contact_nom_usage = "' . $nomUsage . '",
+				  			contact_prenoms = "' . $prenoms . '"
+				  WHERE		contact_id = ' . $_GET['fiche'];
 		// On exécute la requête
 		$db->query($query);
+	}
 
-	endif;
-	
-	// On redirige
 	$core->tpl_go_to('fiche', array('id' => $_GET['fiche']), true);
 ?>
