6babc1bba774745556c67e5c1de453ccf6bc6c6c
Hiwelo <mail@damiensenger.me>
2014-10-03 18:50:40 +0200

    Documentation de la classe CSV

--- class/csv.class.php
+++ class/csv.class.php
@@ -1,28 +1,29 @@
 <?php
+/**
+ * La classe csv regroupe les méthodes de traitement des fichiers CSV
+ * 
+ * @package		leQG
+ * @author		Damien Senger <mail@damiensenger.me>
+ * @copyright	2014 MSG SAS – LeQG
+ */
 
-/*
-	Classe du noyau de traitement des données CSV du site
-*/
-
-
-class csv extends core {
-	
-// Définition des propriétés
-	private $db; // Lien vers la base MySQL
-	private $url; // Domaine du serveur
+class csv {
 	
 	
-// Définition des méthodes
-	
-	public	function __construct($db, $url) {
-		$this->db = $db;
-		$this->url = $url;
-	}
-		
-	
-// Méthodes liées à la classe sélectionnée
-	
-	// lectureFichier( string , string ) permet de traiter les données d'un fichier CSV dans un tableau
+	/**
+	 * Cette méthode permet la lecture et le traitement d'un fichier CSV
+	 * 
+	 * Cette méthode permet d'ouvrir un fichier CSV, de récupérer l'ensemble de 
+	 * son contenu et de le retourner sous forme d'un tableau PHP.
+	 * 
+	 * @author	Damien Senger <mail@damiensenger.me>
+	 * @version	1.0
+	 *
+	 * @param	string	$fichier	URL du fichier CSV à traiter
+	 * @param	string	$separateur	Type de séparateur utilisé dans le fichier CSV
+	 * @return	array				Tableau contenant le contenu du fichier CSV
+	 */
+	 
 	public	function lectureFichier( $fichier , $separateur = ';' ) {
 		
 		// On défini les données de démarrage
