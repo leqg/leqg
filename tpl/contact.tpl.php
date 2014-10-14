8142c0cb33c8c7c98b0e2e37a2cd19c45e1e299c
Hiwelo <mail@damiensenger.me>
2014-10-14 17:51:05 +0200

    Ajout de contrôleurs

--- tpl/contact.tpl.php
+++ tpl/contact.tpl.php
@@ -19,9 +19,10 @@
 	
 		<h4>Données connues</h4>
 		<ul class="etatcivil">
-			<li class="naissance"><?php echo $contact->naissance(); ?></li>
-			<li class="age"><?php echo $contact->age(); ?></li>
+			<li class="naissance"><?php if ($contact->contact['contact_naissance_date'] != '0000-00-00') { echo $contact->naissance(); } else { echo '<span class="inconnu">Date de naissance inconnue</span>'; } ?></li>
+			<li class="age"><?php if ($contact->contact['contact_naissance_date'] != '0000-00-00') { echo $contact->age(); } else { echo '<span class="inconnu">Âge inconnu</span>'; } ?></li>
 			<?php if ($contact->contact['adresse_id']) { ?><li class="adresse"><?php echo $contact->adresse('declaree'); ?></li><?php } ?>
+			<?php if (!empty($contact->contact['contact_organisme'])) { ?><li class="organisme"><?php echo $contact->contact['contact_organisme']; ?> <?php if (!empty($contact->contact['contact_fonction'])) { echo $contact->contact['contact_fonction']; } ?></li><?php } ?>
 		</ul>
 		
 		<?php if ($contact->contact['contact_electeur'] == 1) : ?>
