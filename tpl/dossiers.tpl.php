c60a199d0603bf322b0d16d8add9dca6e445de49
Damien Senger <mail@damiensenger.me>
2014-11-01 20:46:35 +0100

    Dossiers – mise en place

---
+++ tpl/dossiers.tpl.php
@@ -0,0 +1,9 @@
+<?php $core->tpl_header(); ?>
+	
+	<h2>Dossiers en cours</h2>
+	
+	<section class="icone" id="aucunDossier">
+		<h3>Aucun dossier ouvert actuellement.</h3>
+	</section>
+	
+<?php $core->tpl_footer(); ?>
