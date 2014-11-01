2f6e5f7e69ca61754f3ad2ccdc3f92871f93d83b
Damien Senger <mail@damiensenger.me>
2014-11-01 20:26:31 +0100

    Contacts – nouveau moteur

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -1,11 +1,63 @@
 var contacts = function() {
+
+	// Fonction de recherche et d'affichage des fiches selon la recherche
+	function listing()
+	{
+		// On récupère le tri actuel
+		var triActu = $('#summaryTri').val();
+		var premiereFiche = $('#premiereFiche').val();
+		
+		// On affiche un message d'attente
+		var messageAttente = 'Chargement des fiches en cours…';
+		$('.majListeFiches').html('<tr><td colspan="6" class="messageAttente">' + messageAttente + '</td></tr>');
+		
+		// On récupère la liste JSON des fiches à afficher
+		$.getJSON('ajax.php?script=contacts', { tri: triActu, debut: premiereFiche }, function(data) {
+			
+			// On va commencer par vider le tableau
+			$('.majListeFiches').html('');
+			
+			// On va traiter les données JSON des fiches à afficher
+			$.each(data, function(key, val) {
+				// On va créer tout d'abord une ligne dans le tableau
+				$('.majListeFiches').append('<tr class="contact-' + val.contact_id + '"><td>&nbsp;</td></tr>');
+				
+				// On ajoute maintenant les colonnes pour chaque lignes
+				// à commencer par le nom
+				$('.majListeFiches tr.contact-' + val.contact_id).append('<td class="nom"><a href="index.php?page=contact&contact=' + val.contact_md5 + '">' + val.nom_affichage + '</a></td>');
+				
+				// On affiche les coordonnées
+				$('.majListeFiches tr.contact-' + val.contact_id).append('<td class="email">' + val.email + '</td>');
+				$('.majListeFiches tr.contact-' + val.contact_id).append('<td class="mobile">' + val.mobile + '</td>');
+				$('.majListeFiches tr.contact-' + val.contact_id).append('<td class="fixe">' + val.fixe + '</td>');
+				
+				// On transforme la liste des tags en puces
+				var puces = '<ul class="tags">';
+				$.each(val.tags, function(k, v) {
+					puces = puces + '<li class="tag">' + v + '</li>';
+				});
+				puces = puces + '</ul>';
+				
+				// On affiche la liste des tags
+				$('.majListeFiches tr.contact-' + val.contact_id).append('<td class="listeTags">' + puces + '</td>');
+			});
+		});
+		
+		return true;		
+	}
 	
+	
+	// Au chargement, on exécute la fonction
+	listing();
+
+
 	// Comportement au clic sur l'ajout de critère
 	$('.selectionCritere').click(function() {
 		var critere = $(this).data('critere');
 		$('.detail-critere').hide();
 		$('.detail-critere-' + critere).show();
 	});
+	
 	
 	// Comportement à la validation de l'ajout de critère
 	$('#form-ajoutCritere').submit(function() {
@@ -39,46 +91,15 @@ var contacts = function() {
 		
 		// On ajout les nouveaux critères de tri dans le input[type=hidden] de résumé
 		$('#summaryTri').val(triActu);
-		
-		// On retire les fiches affichées dans le tableau pour y mettre un message d'attente
-		var messageAttente = 'Chargement des fiches en cours...';
-		$('#majListeFiches').html('<tr><td colspan="6" class="messageAttente">' + messageAttente + '</td></tr>');
-		
+				
 		// On ajoute le tag à la liste des critères
 		$('#criteres').append('<span class="tag" data-critere="' + critere + ':' + tri + '">' + critere + ':' + affichage + '</span>');
 		
 		// On ferme la fenètre
 		$('.overlayForm').fadeOut();
 		
-		// On récupère la liste JSON des fiches à afficher
-		$.getJSON(action, { 'tri': triActu }, function(data){
-			// On va traiter les données JSON pour afficher chaque ligne du tableau :)
-			$('#majListeFiches').html('');
-			$.each(data, function() {
-				// On retraite certaines données
-				if (this['email'] == null) { this['email'] = '&nbsp;'; }
-				if (this['mobile'] == null) { this['mobile'] = '&nbsp;'; } else { this['mobile'] = this['mobile'].substr(0, 2) + ' ' + this['mobile'].substr(2, 2) + ' ' +  this['mobile'].substr(4, 2) + ' ' +  this['mobile'].substr(6, 2) + ' ' +  this['mobile'].substr(8, 2); }
-				if (this['telephone'] == null) { this['telephone'] = '&nbsp;'; } else { this['telephone'] = this['telephone'].substr(0, 2) + ' ' + this['telephone'].substr(2, 2) + ' ' +  this['telephone'].substr(4, 2) + ' ' +  this['telephone'].substr(6, 2) + ' ' +  this['telephone'].substr(8, 2); }
-				if (this['tags'] == null) { this['tags'] = '&nbsp;'; }
-			
-				var col1 = '<td><div class="radio"><input type="checkbox" name="fiche-' + this['id'] + '" id="fiche-' + this['id'] + '"><label for="fiche-' + this['id'] + '"><span><span></span></span></label></div></td>';
-				var col2 = '<td><a href="index.php?page=fiche&id=' + this['id'] + '">' + this['nom'] + ' ' + this['nom_usage'] + ' ' + this['prenoms'] + '</a></td>';
-				var col3 = '<td>' + this['email'] + '</td>';
-				var col4 = '<td>' + this['mobile'] + '</td>';
-				var col5 = '<td>' + this['telephone'] + '</td>';
-				var col6 = '<td>' + this['tags'] + '</td>';
-				
-				$('#majListeFiches').append('<tr>' + col1 + col2 + col3 + col4 + col5 + col6 + '</tr>');
-			});
-			
-			// On fini par reformater la taille des colonnes
-			$('table#listeFiches tbody td:nth-of-type(1)').width(colonne1 - 2);
-			$('table#listeFiches tbody td:nth-of-type(2)').width(colonne2);
-			$('table#listeFiches tbody td:nth-of-type(3)').width(colonne3 - 5);
-			$('table#listeFiches tbody td:nth-of-type(4)').width(colonne4);
-			$('table#listeFiches tbody td:nth-of-type(5)').width(colonne5);
-			$('table#listeFiches tbody td:nth-of-type(6)').width(colonne6 - 3);
-		});
+		// On recherche les fiches et on les affiche
+		listing();
 				
 		// On évite le lancement du formulaire
 		return false;
--- tpl/contacts.tpl.php
+++ tpl/contacts.tpl.php
@@ -25,14 +25,15 @@
 				</tr>
 			</thead>
 			
-			<tbody id="majListeFiches"></tbody>
+			<tbody class="majListeFiches"></tbody>
 		</table>
 	</section>
 </section>
 
 <div id="ajoutCritere" class="overlayForm">
 	<form method="post" action="ajax.php?script=contacts-liste" id="form-ajoutCritere">
-		<input type="hidden" name="summaryTri" id="summaryTri" value="contact:tous">
+		<input type="hidden" name="summaryTri" id="summaryTri" value="">
+		<input type="hidden" name="premiereFiche" id="premiereFiche" value="0">
 		<a class="fermetureOverlay" data-overlay="ajoutCritere" href="#">&#xe813;</a>
 		<h3>Ajout d'un critère de tri</h3>
 		<ul>
@@ -77,50 +78,3 @@
 </div>
 
 <div id="exportation-lancee">L'exportation a été lancée, vous recevrez le fichier demandé par email.</div>
-
-
-
-
-
-
-
-
-<section id="contacts-ancien" style="display:none;">
-	<div id="historique">
-		<h3>Dernières interactions</h3>
-		
-		<ul class="timeline debutNow">
-			<?php $interactions = $historique->dernieresInteractions(15); if (count($interactions) > 0) : foreach ($interactions as $interaction) : ?>
-			<li class="<?php echo $interaction['type']; ?>">
-				<strong><a href="<?php $core->tpl_go_to('fiche', array('id' => $interaction['contact_id'], 'interaction' => $interaction['id'])); ?>" class="nostyle"><?php if (empty($interaction['objet'])) { echo $historique->returnType($interaction['type']); } else { echo $interaction['objet']; } ?></a></strong>
-				<ul>
-					<li class="contact"><a href="<?php $core->tpl_go_to('fiche', array('id' => $interaction['contact_id'])); ?>"><?php $fiche->affichageNomByID($interaction['contact_id']); ?></a></li>
-					<li class="date"><?php echo date('d/m/Y', strtotime($interaction['date'])); ?></li>
-					<li class="lieu"><?php echo $interaction['lieu']; ?></li>
-				</ul>
-			</li>
-			<?php endforeach; endif; ?>
-			<li class="fin"></li>
-		</ul>
-	</div><!--
-	
- --><div id="taches">
-		<h3>Dernières tâches créées</h3>
-		
-		<ul class="timeline debutNow">
-			<?php $taches = $tache->liste(15); if (count($taches) > 0) : foreach ($taches as $t) : ?>
-			<li class="tache">
-				<?php $interaction = $historique->recherche($t['historique_id']); ?>
-				<strong><?php echo $t['description']; ?></strong>
-				<?php if (!empty($t['historique_id']) && $t['historique_id'] > 0) : ?>
-				<ul>
-					<li class="contact"><a href="<?php $core->tpl_go_to('fiche', array('id' => $interaction['contact_id'])); ?>"><?php $fiche->affichageNomByID($interaction['contact_id']); ?></a></li>
-					<li class="date"><?php echo date('d/m/Y', strtotime($t['creation'])); ?></li>
-				</ul>
-				<?php endif; ?>
-			</li>
-			<?php endforeach; endif; ?>
-			<li class="fin"></li>
-		</ul>
-	</div>
-</section>
