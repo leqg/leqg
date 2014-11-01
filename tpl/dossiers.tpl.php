5f33947bf79ee72444d374158976a01f16de7be1
Damien Senger <mail@damiensenger.me>
2014-11-01 20:57:11 +0100

    Dossiers – modification du texte d'interface

--- tpl/dossiers.tpl.php
+++ tpl/dossiers.tpl.php
@@ -3,7 +3,7 @@
 	<h2>Dossiers en cours</h2>
 	
 	<section class="icone" id="aucunDossier">
-		<h3>Aucun dossier ouvert actuellement.</h3>
+		<h3>Aucun dossier ouvert actuellement.<br>Démarrez un dossier à travers un événement existant.</h3>
 	</section>
 	
 <?php $core->tpl_footer(); ?>
