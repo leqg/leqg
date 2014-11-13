cb566a17d7e5bc5cb629209c53e93baa876767da
Damien Senger <mail@damiensenger.me>
2014-11-13 15:59:23 +0100

    Debug Encodage Table Régions

--- test.php
+++ test.php
@@ -12,7 +12,7 @@ require_once('includes.php');
 $link = new PDO("mysql:host=" . Configuration::read('db.host') . ";dbname=" . Configuration::read('db.basename') . ";charset=utf8", Configuration::read('db.user'), Configuration::read('db.pass'));
 
 // On va retraiter toutes les données à "problème" présentes dans la BDD en commençant par les contacts
-$query = $link->prepare('SELECT * FROM `mission` WHERE `mission_nom` LIKE :terme ORDER BY `mission_id` ASC');
+$query = $link->prepare('SELECT * FROM `regions` WHERE `region_nom` LIKE :terme ORDER BY `region_id` ASC');
 $terme = "%&%";
 $query->bindParam(':terme', $terme);
 $query->execute();
@@ -28,16 +28,16 @@ foreach ($resultats as $resultat)
 {
 	
 	// On retraite les données à problème
-	$resultat['mission_nom'] = retraitement($resultat['mission_nom']);
+	$resultat['region_nom'] = mb_convert_case(retraitement($resultat['region_nom']), MB_CASE_TITLE);
 	
 	// On prépare la requête de modification
-	$query = $link->prepare('UPDATE `mission` SET `mission_nom` = :nom WHERE `mission_id` = :id');
-	$query->bindParam(':nom', $resultat['mission_nom']);
-	$query->bindParam(':id', $resultat['mission_id']);
+	$query = $link->prepare('UPDATE `regions` SET `region_nom` = :nom WHERE `region_id` = :id');
+	$query->bindParam(':nom', $resultat['region_nom']);
+	$query->bindParam(':id', $resultat['region_id']);
 	
 	$query->execute();
 	
-	echo $resultat['mission_id'] . '<br>';
+	echo $resultat['region_id'] . '<br>';
 }
 
 
