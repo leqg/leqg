bba067a5237a91b9e4c575b47937e8112f72d1a4
Damien Senger <mail@damiensenger.me>
2014-11-17 15:47:37 +0100

    Boitage – amélioration de compatibilité

--- class/boitage.class.php
+++ class/boitage.class.php
@@ -7,7 +7,7 @@
  * @copyright	2014 MSG SAS – LeQG
  */
 
-class boitage extends core {
+class Boitage {
 	
 	/**
 	 * @var	object  $db   Propriété contenant le lien vers la base de données de l'utilisateur
