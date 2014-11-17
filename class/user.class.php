69db55eeb25e618fb7b9ec6ffdd79c2486d0b794
Damien Senger <mail@damiensenger.me>
2014-11-17 04:31:37 +0100

    Debug

--- class/user.class.php
+++ class/user.class.php
@@ -413,7 +413,7 @@ class User extends core {
 	public static function protection($auth = 1) {
 		// On prépare la connexion à la BDD
 		$link = Configuration::read('db.core');
-		Core::debug($_COOKIE);
+		
 		// On regarde si un cookie existe
 		if (isset($_COOKIE['leqg'], $_COOKIE['time']) && !empty($_COOKIE['time']) && !empty($_COOKIE['leqg'])) {
 			
@@ -475,6 +475,7 @@ class User extends core {
 		else {
 			setcookie('leqg', '', time()-1);
 			setcookie('time', '', time()-1);
+			Core::debug($_COOKIE);
 			header('Location: http://auth.leqg.info');
 		}	
 	}
