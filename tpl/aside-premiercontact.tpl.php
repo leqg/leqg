fae795407e941ca02030e80551c6d0ff62cb33b2
Damien Senger <mail@damiensenger.me>
2014-11-18 16:08:30 +0100

    Nettoyage des fichiers du QG

--- tpl/aside-premiercontact.tpl.php
+++
@@ -1,3 +0,0 @@
-<div id="premierContact">
-	<a id="ajoutInteraction" class="nostyle" href="<?php $core->tpl_go_to('fiche', array('id' => $_GET['id'])); ?>&nouvelleInteraction=true">Ajouter une première interaction</a>
-</div>
