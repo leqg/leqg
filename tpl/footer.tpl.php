95e23f4b17d456351308ed712948ed7883a549e1
Damien Senger <hiwelo@users.noreply.github.com>
2014-11-18 01:07:45 +0100

    Suppression temporaire du centre de notification

--- tpl/footer.tpl.php
+++ tpl/footer.tpl.php
@@ -2,19 +2,7 @@
 	
 	<!-- Volet de notification -->
 	<aside id="notificationCenter">
-	<?php if ($notification->nombre() == -2) : ?>
-		<h6>Tâches à réaliser</h6>
-		<ul class="listeIcone">
-			<?php $taches = $tache->recherche($user->get_the_id()); foreach ($taches as $t) : ?>
-			<li class="tache">
-				<strong><?php echo $t['description']; ?></strong>
-				<?php if (!is_null($t['deadline'])) { ?><em>Deadline <?php echo date('d/m/Y', $t['deadline']) ;?></em><?php } ?>
-			</li>
-			<?php endforeach; ?>
-		</ul>
-	<?php else : ?>
 		<div id="vide">Aucune notification</div>
-	<?php endif; ?>
 	</aside>
 </body>
 </html>
