2df8b290ccd1ccd4a57ba362d710251fbb2af393
Damien Senger <mail@damiensenger.me>
2014-11-18 14:46:47 +0100

    SweetAlert – mise en place

--- tpl/header.tpl.php
+++ tpl/header.tpl.php
@@ -7,6 +7,7 @@
 	<!-- lien vers les feuilles de style -->
 	<link rel="stylesheet" media="all" href="assets/css/main.css">
 	<link rel="stylesheet" media="all" href="assets/css/fonts.css">
+	<link rel="stylesheet" media="all" href="assets/css/sweet-alert.css">
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic,500,500italic,700,700italic,900,900italic&subset=latin-ext">
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Fira+Sans:300,300italic,400,400italic,500,500italic,700,700italic&subset=latin-ext">
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,200italic,300,300italic,400,400italic,600,600italic,700,700italic,900,900italic&subset=latin-ext">
@@ -17,6 +18,7 @@
 	<script src="assets/js/jquery-2.1.1.min.js"></script>
 	<script src="https://api.tiles.mapbox.com/mapbox.js/v2.1.4/mapbox.js"></script>
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDSkeqzB0suNWsj8fU3If9tA0spIl_xN2A&sensor=false"></script>
+	<script src="assets/js/sweet-alert.min.js"></script>
 	<script src="assets/js/main.js"></script>
 	<?php if (isset($_GET['page'])) { ?><script src="assets/js/<?php echo $_GET['page']; ?>.js"></script><?php } ?>
 </head>
