7734826f6f3c489d462e3954bac16fc2c0ee41d0
Damien Senger <mail@damiensenger.me>
2014-11-19 11:06:09 +0100

    SMS + Emailing – RAZ pour mise en conformité

--- tpl/email.tpl.php
+++
@@ -1,7 +0,0 @@
-<table id="services">
-	<tr>
-		<td><a href="<?php $core->tpl_go_to('email', array('action' => 'nouveau')); ?>"><span>&#xe8ef;</span><p>Nouvel envoi groupé</p></a></td>
-		<td><a href="<?php $core->tpl_go_to('email', array('action' => 'historique')); ?>"><span>&#xe8dd;</span><p>Historique des envois</p></a></td>
-		<td><a href="<?php $core->tpl_go_to('email', array('action' => 'reglages')); ?>"><span>&#xe856;</span><p>Réglages</p></a></td>
-	</tr>
-</table>
