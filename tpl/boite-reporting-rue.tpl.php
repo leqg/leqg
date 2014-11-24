7258a3d4ae5965359f651aab3392b940ca61c7a2
Damien Senger <mail@damiensenger.me>
2014-11-24 21:00:21 +0100

    Boisage – ajout d'une carte aux missions

--- tpl/boite-reporting-rue.tpl.php
+++ tpl/boite-reporting-rue.tpl.php
@@ -2,6 +2,8 @@
 
     <h2 class="titre" data-mission="<?php echo md5($mission['mission_id']); ?>">Mission &laquo;&nbsp;<?php echo $mission['mission_nom']; ?>&nbsp;&raquo;</h2>
 
+	<section id="mapbox-mission"></section>
+
     <section class="mission-porte">        
     		<?php if (Boite::nombreImmeubles($mission['mission_id'])) : ?>
     		    <?php $rues = Boite::liste($mission['mission_id']); foreach ($rues as $rue => $immeubles) : if (count($immeubles)) : if ($rue == $_GET['rue']) : ?>
@@ -46,5 +48,64 @@
         <a href="<?php Core::tpl_go_to('boite', array('action' => 'voir', 'mission' => $mission['mission_id'])); ?>" class="nostyle"><button>Revenir à la mission</button></a>
         <?php endif; ?>
     </section>
+    
+	<?php
+		if (Boite::nombreImmeubles($mission['mission_id'])) :
+		
+	    	$rues = Boite::liste($mission['mission_id']);
+	    	
+	    	foreach ($rues as $rue => $immeubles) :
+	    	
+	    		if (count($immeubles)) :
+	    		
+	    			if ($rue == $_GET['rue']) :
+	    			
+	    				$ville = Carto::ville(Carto::villeParRue($rue));
+	?>
+					    <script>
+							// Mise en place de la map
+							var map = L.map('mapbox-mission');
+							
+							// Sélection du tile layer OSM
+							L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+						
+							<?php foreach ($immeubles as $immeuble) : ?>
+							// On récupère sur le Nominatim OSM les coordonnées de la rue en question
+							var data = {
+								format: 'json',
+								email: 'tech@leqg.info',
+								country: 'France',
+								city: "<?php echo $ville['commune_nom']; ?>",
+								street: "<?php Carto::afficherImmeuble($immeuble); echo $nomRue; ?>"
+							}
+							
+							// On récupère le JSON contenant les coordonnées de la rue
+							$.getJSON('https://nominatim.openstreetmap.org', data, function(data) {
+								// On récupère uniquement les données du premier résultat
+								data = data[0];
+								
+								// On prépare la boundingbox
+								var loc1 = new L.LatLng(data.boundingbox[0], data.boundingbox[2]);
+								var loc2 = new L.LatLng(data.boundingbox[1], data.boundingbox[3]);
+								var bounds = new L.LatLngBounds(loc1, loc2);
+								
+								// On fabrique une vue qui contient l'ensemble du secteur demandé
+								map.fitBounds(bounds, { maxZoom: 17 });
+								
+								// On ajoute un marker au milieu de la rue
+								L.marker([data.lat, data.lon], {
+									clicable: false,
+									title: "<?php Carto::afficherImmeuble($immeuble); echo $nomRue; ?>"
+								}).addTo(map);
+							});
+							<?php endforeach; ?>
+						</script>
+	<?php
+					endif;
+				endif;
+			endforeach;
+		endif;
+	?>
+
     
 <?php Core::tpl_footer(); ?>
