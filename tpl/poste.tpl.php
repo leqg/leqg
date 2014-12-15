415a6a14fa2bb5b95fbbba767a58f2ebeee30860
Damien Senger <mail@damiensenger.me>
2014-12-15 18:39:27 +0100

    Publipostage – retrait de l'ancien module

--- tpl/poste.tpl.php
+++
@@ -1,6 +0,0 @@
-<table id="services">
-	<tr>
-		<td><a href="<?php $core->tpl_go_to('poste', array('action' => 'nouveau')); ?>"><span>&#xe8ef;</span><p>Préparer un publipostage</p></a></td>
-		<td><a href="<?php $core->tpl_go_to('poste', array('action' => 'historique')); ?>"><span>&#xe8dd;</span><p>Historique des préparations</p></a></td>
-	</tr>
-</table>
