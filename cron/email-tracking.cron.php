998fd51f738ab70fd8296e28da95618edcf41ed4
Damien Senger <mail@damiensenger.me>
2015-03-06 11:40:46 +0100

    Email tracking update

--- cron/email-tracking.cron.php
+++ cron/email-tracking.cron.php
@@ -5,7 +5,7 @@ $mandrill = Configuration::read('mail');
 
 // on recherche tous les emails envoyés 
 $link = Configuration::read('db.link');
-$query = $link->prepare('SELECT * FROM `tracking` WHERE `id` != "" AND `control` = 0 ORDER BY `time` ASC LIMIT 0, 150');
+$query = $link->prepare('SELECT * FROM `tracking` WHERE `status` != "pending" AND `control` = 0 ORDER BY `time` ASC LIMIT 0, 150');
 $query->execute();
 $datas = $query->fetchAll(PDO::FETCH_ASSOC);
 
@@ -14,15 +14,33 @@ $query = $link->prepare('UPDATE `tracking` SET `opens` = :opens, `status` = :sta
 foreach ($datas as $data) {
     $result = $mandrill->messages->info($data['id']);
     
+    switch($result['state']) {
+        case 'deferred':
+            $result['state'] = 'queued';
+            break;
+        
+        case 'hard-bounced':
+            $result['state'] = 'rejected';
+            break;
+        
+        case 'soft-bounced':
+            $result['state'] = 'rejected';
+            break;
+        
+        case 'bounced':
+            $result['state'] = 'rejected';
+            break;
+    }
     $query->bindParam(':opens', $result['opens']);
     $query->bindParam(':status', $result['state']);
     $query->bindParam(':id', $result['_id']);
     $query->execute();
     
     if ($result['state'] == 'rejected') {
-        $query = $link->prepare('UPDATE `tracking` SET `reject_reason` = :reason WHERE `id` = :id');
-        $query->bindValue(':reason', $result['smtp_events'][0]['type']);
-        $query->bindValue(':id', $result['_id']);
-        $query->execute();
+        $query2 = $link->prepare('UPDATE `tracking` SET `status` = "reject", `reject_reason` = :reason, `reject_msg` = :msg WHERE `id` = :id');
+        $query2->bindValue(':reason', $result['bounce_description']);
+        $query2->bindValue(':msg', $result['diag']);
+        $query2->bindValue(':id', $result['_id']);
+        $query2->execute();
     }
 }
