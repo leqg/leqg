a39a687753ddd774be0be4d5042bea606e547781
Damien Senger <mail@damiensenger.me>
2015-03-06 10:53:39 +0100

    Email tracking cron update

--- cron/email-tracking.cron.php
+++ cron/email-tracking.cron.php
@@ -5,15 +5,15 @@ $mandrill = Configuration::read('mail');
 
 // on recherche tous les emails envoyés 
 $link = Configuration::read('db.link');
-$query = $link->prepare('SELECT * FROM `tracking` WHERE `status` = "queued"');
+$query = $link->prepare('SELECT * FROM `tracking` WHERE `id` != "" AND `control` = 0 ORDER BY `time` ASC LIMIT 0, 150');
 $query->execute();
 $datas = $query->fetchAll(PDO::FETCH_ASSOC);
 
-$query = $link->prepare('UPDATE `tracking` SET `opens` = :opens, `status` = :status WHERE `id` = :id');
+$query = $link->prepare('UPDATE `tracking` SET `opens` = :opens, `status` = :status, `control` = 1 WHERE `id` = :id');
 
 foreach ($datas as $data) {
     $result = $mandrill->messages->info($data['id']);
-    
+
     $query->bindParam(':opens', $result['opens']);
     $query->bindParam(':status', $result['state']);
     $query->bindParam(':id', $result['_id']);
