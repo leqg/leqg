f2cd2523eab06d129c2e69ce33f82f1c302b3890
Damien Senger <hi@hiwelo.co>
2016-01-20 16:43:30 +0100

    PHPCS lint configuration class

--- class/configuration.class.php
+++ class/configuration.class.php
@@ -1,59 +1,62 @@
 <?php
 /**
- * LeQG – political database manager
- * Configuration class
- * 
- * PHP Version 5.5.21
- * 
- * @category  LeQG
- * @package   LeQG
- * @author    Damien Senger <hi@hiwelo.co>
- * @copyright 2014-2015 MSG SAS – LeQG
- * @license   Personal Use Only
- * @version   GIT:<git@github.com:hiwelo/leqg.git>
- * @link      http://hiwelo.co/
- * */
+ * Gestion de la configuration
+ *
+ * PHP version 5
+ *
+ * @category Configuration
+ * @package  LeQG
+ * @author   Damien Senger <hi@hiwelo.co>
+ * @license  https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License 3.0
+ * @link     http://leqg.info
+ */
 
 /**
- * LeQG – political database manager
- * Configuration class
- * 
- * PHP Version 5.5.21
- * 
- * @category  LeQG
- * @package   LeQG
- * @author    Damien Senger <hi@hiwelo.co>
- * @copyright 2014-2015 MSG SAS – LeQG
- * @license   Personal Use Only
- * @link      http://hiwelo.co/
- * */
+ * Gestion de la configuration
+ *
+ * PHP version 5
+ *
+ * @category Configuration
+ * @package  LeQG
+ * @author   Damien Senger <hi@hiwelo.co>
+ * @license  https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License 3.0
+ * @link     http://leqg.info
+ */
 class Configuration
 {
-    
     /**
-     * @var array $confArray Configuration data array
-     * */
-    static $confArray;
-    
+     * Configuration data array
+     *
+     * @var array
+     *
+     * @static
+     */
+    static public $confArray = [];
+
     /**
-         * Reading configuration method
-         * @param string $name Saved conf data name
-         * @result mixed
-         * */
-    public static function read($name)
+     * Reading configuration method
+     *
+     * @param string $name data to read
+     *
+     * @return mixed
+     * @static
+     */
+    static public function read(string $name)
     {
         return self::$confArray[$name];
     }
-    
+
     /**
-         * Writing configuration method
-         * @param string $name  To save data name
-         * @param mixed  $value To save data content
-         * @result void
-         * */    
-    public static function write($name, $value)
+     * Writing configuration method
+     *
+     * @param string $name  Data name
+     * @param mixed  $value Data value
+     *
+     * @return void
+     * @static
+     */
+    static public function write(string $name, $value)
     {
         self::$confArray[$name] = $value;
     }
-    
 }
