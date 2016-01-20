111d290e920daa2b224834d85c045f148bd1b1b5
Damien Senger <hi@hiwelo.co>
2016-01-20 18:05:13 +0100

    PHPCS lint folder class

--- class/folder.class.php
+++ class/folder.class.php
@@ -1,48 +1,43 @@
 <?php
 /**
- * LeQG – political database manager
- * Folder class
- * 
- * PHP Version 5.5.21
- * 
- * @category  LeQG
- * @package   LeQG
- * @author    Damien Senger <hi@hiwelo.co>
- * @copyright 2014-2015 MSG SAS – LeQG
- * @license   Personal Use Only
- * @version   GIT:<git@github.com:hiwelo/leqg.git>
- * @link      http://hiwelo.co/
- * */
+ * Event's folder class
+ *
+ * PHP version 5
+ *
+ * @category Folder
+ * @package  LeQG
+ * @author   Damien Senger <hi@hiwelo.co>
+ * @license  https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License 3.0
+ * @link     http://leqg.info
+ */
 
 /**
- * LeQG – political database manager
- * Folder class
- * 
- * PHP Version 5.5.21
- * 
- * @category  LeQG
- * @package   LeQG
- * @author    Damien Senger <hi@hiwelo.co>
- * @copyright 2014-2015 MSG SAS – LeQG
- * @license   Personal Use Only
- * @link      http://hiwelo.co/
- * */
+ * Event's folder class
+ *
+ * PHP version 5
+ *
+ * @category Folder
+ * @package  LeQG
+ * @author   Damien Senger <hi@hiwelo.co>
+ * @license  https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License 3.0
+ * @link     http://leqg.info
+ */
 class Folder
 {
-    
     /**
-     * @var     array   $_folder    Folder data
-     * */
-    private $_folder;
-    
-    
+     * Folder data
+     * @var array
+     */
+    private $_folder = [];
+
     /**
      * Construct method
-     * 
-     * @param  string $folder Folder ID (w/o hash)
-     * @result void
-     * */
-    public function __construct($folder)
+     *
+     * @param integer $folder Folder ID (w/o hash)
+     *
+     * @return void
+     **/
+    public function __construct(int $folder)
     {
         $query = Core::query('folder-data');
         $query->bindParam(':folder', $folder, PDO::PARAM_INT);
@@ -50,73 +45,74 @@ class Folder
         $this->_folder = $query->fetch(PDO::FETCH_ASSOC);
         $this->_folder['md5'] = md5($this->_folder['id']);
     }
-    
-    
+
     /**
      * Get an asked information
-     * 
-     * @param  string $data Asked information
+     *
+     * @param string $data Asked information
+     *
      * @return mixed
-     * */
-    public function get($data)
+     **/
+    public function get(string $data)
     {
         return $this->_folder[$data];
     }
-    
-    
+
     /**
      * Update an asked information
-     * 
-     * @param  string $data  Asked information
-     * @param  string $value New value
-     * @result void
-     * */
-    public function update($data, $value)
+     *
+     * @param string $data  Asked information
+     * @param string $value New value
+     *
+     * @return void
+     **/
+    public function update(string $data, string $value)
     {
         $link = Configuration::read('db.link');
-        $query = $link->prepare('UPDATE `dossiers` SET `'.$data.'` = :value WHERE `dossier_id` = :id');
+        $sql = 'UPDATE `dossiers`
+                SET `' . $data . '` = :value
+                WHERE `dossier_id` = :id';
+        $query = $link->prepare($sql);
         $query->bindValue(':id', $this->_folder['id'], PDO::PARAM_INT);
         $query->bindValue(':value', $value);
         $query->execute();
     }
-    
-    
+
     /**
      * Export known informations in JSON
-     * 
+     *
      * @return string
-     * */
+     **/
     public function json()
     {
         return json_encode($this->_folder);
     }
-    
-    
+
     /**
      * List of all events linked to the folder
-     * 
+     *
      * @return array
-     * */
+     **/
     public function events()
     {
         $query = Core::query('folder-events');
         $query->bindValue(':folder', $this->_folder['id'], PDO::PARAM_INT);
         $query->execute();
-        
+
         $events = array();
         $_events = $query->fetchAll(PDO::FETCH_NUM);
-        foreach ($_events as $event) { $events[] = $event[0]; 
+        foreach ($_events as $event) {
+            $events[] = $event[0];
         }
-        
+
         return $events;
     }
-    
-    
+
     /**
      * List of all tasks linked to the folder
-     * 
+     *
      * @return array
-     * */
+     **/
     public function tasks()
     {
         $query = Core::query('folder-tasks');
@@ -124,13 +120,12 @@ class Folder
         $query->execute();
         return $query->fetchAll(PDO::FETCH_ASSOC);
     }
-    
-    
+
     /**
      * List of all tasks linked to the folder
-     * 
+     *
      * @return array
-     * */
+     **/
     public function files()
     {
         $query = Core::query('folder-files');
@@ -138,38 +133,39 @@ class Folder
         $query->execute();
         return $query->fetchAll(PDO::FETCH_ASSOC);
     }
-    
-    
+
     /**
      * Create a new folder
-     * 
-     * @param  string $name Folder's name
-     * @param  string $desc Folder's description
-     * @result int                 New folder ID
-     * */
-    public static function create($name, $desc)
+     *
+     * @param string $name Folder's name
+     * @param string $desc Folder's description
+     *
+     * @return integer New folder ID
+     * @static
+     **/
+    static public function create(string $name, string $desc)
     {
         $query = Core::query('folder-new');
         $query->bindValue(':name', $name);
         $query->bindValue(':desc', $desc);
         $query->execute();
-        
+
         return Configuration::read('db.link')->lastInsertId();
     }
-    
-    
+
     /**
      * List of all folders
-     * 
-     * @param  int $status Status of asked folders (1 open 0 close)
+     *
+     * @param integer $status Status of asked folders (1 open 0 close)
+     *
      * @return array
-     * */
-    public static function all($status = 1)
+     * @static
+     **/
+    static public function all($status = 1)
     {
         $query = Core::query('folder-all');
         $query->bindValue(':status', $status, PDO::PARAM_INT);
         $query->execute();
         return $query->fetchAll(PDO::FETCH_ASSOC);
     }
-    
 }
