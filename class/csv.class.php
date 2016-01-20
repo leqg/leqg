bb784d04c54ae00a50987d8e3f9a3d320e5b6032
Damien Senger <hi@hiwelo.co>
2016-01-20 17:44:49 +0100

    PHPCS lint CSV class

--- class/csv.class.php
+++ class/csv.class.php
@@ -1,56 +1,62 @@
 <?php
 /**
- * La classe csv regroupe les méthodes de traitement des fichiers CSV
- * 
- * @package   leQG
- * @author    Damien Senger <mail@damiensenger.me>
- * @copyright 2014 MSG SAS – LeQG
+ * CSV file management
+ *
+ * PHP version 5
+ *
+ * @category CSV
+ * @package  LeQG
+ * @author   Damien Senger <hi@hiwelo.co>
+ * @license  https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License 3.0
+ * @link     http://leqg.info
  */
 
+/**
+ * CSV file management
+ *
+ * PHP version 5
+ *
+ * @category CSV
+ * @package  LeQG
+ * @author   Damien Senger <hi@hiwelo.co>
+ * @license  https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License 3.0
+ * @link     http://leqg.info
+ */
 class Csv
 {
-    
     /**
-     * Cette méthode permet la lecture et le traitement d'un fichier CSV
-     * 
-     * Cette méthode permet d'ouvrir un fichier CSV, de récupérer l'ensemble de 
-     * son contenu et de le retourner sous forme d'un tableau PHP.
-     * 
-     * @author  Damien Senger <mail@damiensenger.me>
-     * @version 1.0
+     * Read a CSV file
      *
-     * @param  string $fichier    URL du fichier CSV à traiter
-     * @param  string $separateur Type de séparateur utilisé dans le fichier CSV
-     * @return array                Tableau contenant le contenu du fichier CSV
+     * @param string $fichier    file name
+     * @param string $separateur CSV fields separator
+     *
+     * @return array
+     * @static
      */
-     
-    public static function lectureFichier( $fichier , $separateur = ';' ) 
+    static public function lectureFichier(string $fichier, $separateur = ';')
     {
-        
         // On défini les données de démarrage
         $row = 0;
         $line = array();
         $data = array();
         $head = array();
-        
+
         // On ouvre le fichier, uniquement en lecture
         $file = fopen($fichier, 'r');
-        
+
         // On calcule la taille du fichier
         $size = filesize($fichier) + 1;
-        
+
         // On fait une boucle pour chaque ligne du fichier
-        while ($line = fgetcsv($file, $size, $separateur)) :
-        
+        while ($line = fgetcsv($file, $size, $separateur)) {
             // On affecte les données de la ligne au tableau général
             $data[$row] = $line;
             $row++;
-        
-        endwhile;
-        
+        }
+
         // On ferme le fichier
         fclose($file);
-        
+
         // On retourne le tableau contenant les données du fichier
         return $data;
     }
